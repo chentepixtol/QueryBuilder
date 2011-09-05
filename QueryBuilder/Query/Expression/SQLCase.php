98f0c0ce8a7cf746ab8d09eec960932b4dd392e6
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2011-09-05 18:34:47 -0500

    SQLCase agregado

---
+++ QueryBuilder/Query/Expression/SQLCase.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Query\Expression;
+
+use Query\Expression;
+
+
+/**
+ *
+ * SQLCase
+ *
+ * @package Query
+ * @subpackage Expression
+ * @copyright (c) Vicente Mendoza <chentepixtol@gmail.com>
+ * @author chentepixtol
+ *
+ */
+class SQLCase extends Expression
+{
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $expression = null;
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $column;
+
+	/**
+	 *
+	 * @var array
+	 */
+	private $cases = array();
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $default = null;
+
+	/**
+	 *
+	 * construct
+	 * @param string $column
+	 * @param array $cases
+	 * @param mixed $default
+	 */
+	public function __construct($column, $cases, $default = null)
+	{
+		$this->column = $column;
+		$this->cases = $cases;
+		$this->default = $default;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function toString()
+	{
+		if( null == $this->expression ){
+			$this->expression = 'CASE '. $this->column;
+			foreach ($this->cases as $when => $then){
+				$this->expression .= " WHEN '{$when}' THEN '{$then}'";
+			}
+			if( $this->default ){
+				$this->expression .= " ELSE '{$this->default}'";
+			}
+			$this->expression .= " END";
+		}
+		return $this->expression;
+	}
+}
